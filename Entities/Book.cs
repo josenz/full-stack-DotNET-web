02ce6a0cfde546bdfb013651e45c578aeadba355
josenz <jose.santos.soares@hotmail.com>
2019-08-13 02:22:00 +1200

    second commit

---
+++ Entities/Book.cs
@@ -0,0 +1,12 @@
+
+namespace BooksAPI.Entities
+{
+    public class Book 
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
