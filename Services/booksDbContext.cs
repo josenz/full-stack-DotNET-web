32a6553651d29990ff5e6b7dff2c656da1931774
josenz <jose.santos.soares@hotmail.com>
2019-08-13 03:12:57 +1200

    third commit

---
+++ Services/booksDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using BooksAPI.Entities;
+
+namespace BooksAPI.Services
+{
+    public class BooksDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public BooksDbContext(DbContextOptions<BooksDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
