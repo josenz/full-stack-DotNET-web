4d8252dd4e87b5e7c2e11215ac913c6d0774f6d9
josenz <jose.santos.soares@hotmail.com>
2019-08-13 20:55:24 +1200

    first

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'wwwroot/dist'),
         filename: 'bundle.js'
+    },
+    modules: {
+        rules: [
+            { test: /\.js?$/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+              }
+            }
+        ]
     }
 };
